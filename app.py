8cc1e359044b16926eec4c6aa319e12933a7918c
kurves <kavssimply@gmail.com>
2023-09-09 16:04:33 +0300

    autoupdate datetime

--- app.py
+++ app.py
@@ -13,36 +13,23 @@ def get_today():
     today=dt.strftime('%A')
     return today
 
-def get_time():
-    while True:
-        t=datetime.utcnow()
-        tt=t.strftime("%Y-%m-%dT%H:%M:%SZ")
-        return tt
-        time.sleep(0)
-    
-
-
-
-user={
-      'slack_name':'Maureen_Mwenswa',
-      'track':'backend',
-        'current_day':get_today(),
-        'utc_time':get_time(),
-        'github_file_url':'https://github.com/kurves/hng-backend-task/blob/main/app.py',
-        'github_repo_url':'https://github.com/kurves/hng-backend-task',
-        'status_code':200
-
-}
 @app.route('/api',methods=['GET'])
+
 def apis():
     request
     slack_name=request.args.get("slack_name")
     track=request.args.get("track")
 
-    return jsonify(
-       user
+    return jsonify({
+    'slack_name':'Maureen_Mwenswa',
+    'track':'backend',
+    'current_day':get_today(),
+    'utc_time':datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
+    'github_file_url':'https://github.com/kurves/hng-backend-task/blob/main/app.py',
+    'github_repo_url':'https://github.com/kurves/hng-backend-task',
+    'status_code':200
 
-    )
+    })
 
 
 if __name__== '__main__':
